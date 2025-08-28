14a03e011c1120d0a1f1ec261a6c8c3cebc0e62d
Sofia <squintero@itba.edu.ar>
2025-08-28 20:18:28 -0300

    hola

---
+++ clase1.js
@@ -0,0 +1,18 @@
+let nombre = "Sofia";
+let edad = 21;
+let profesor = true;
+edad = edad + 1
+
+const PI = 3.14 //constante, no es una variable, no se puede modificar
+//PI += 1
+console.log(PI) //print
+
+const ARRAY1 = [] //el array es tipo un edificio, si pongo cte puedo poner y sacar cosas de adentro siempre y cuando no modifica sus dimensiones
+
+let alumno = {
+    nombre: "Sofia",
+    apellido: "Quintero",
+    edad: 21,
+    deudas: false,
+}
+//array de objetos literales
